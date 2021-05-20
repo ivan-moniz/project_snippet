437d9757903059e788c1df6b9b1f198cbf81d168
ivan-moniz <ivangrandmoniz@gmail.com>
2021-05-20 22:49:49 -0100

    developing project

---
+++ calculator-app-main/script.js
@@ -0,0 +1,32 @@
+const getStylesheet = document.querySelector('link[href="#"]');
+const themeRadioBtns = document.querySelectorAll('.theme-radio')
+
+const changeTheme = (event) => {
+  const target = event.target;
+  if (target.value === "1") {
+    getStylesheet.setAttribute("href", "./dist/css/theme1.min.css");
+    localStorage.setItem("preference", 1);
+    console.log(localStorage.getItem("preference"));
+  } else if (target.value === "2") {
+    getStylesheet.setAttribute("href", "./dist/css/theme2.min.css");
+    localStorage.setItem("preference", 2);
+    console.log(localStorage.getItem("preference"));
+  } else {
+    getStylesheet.setAttribute("href", "./dist/css/theme3.min.css");
+    localStorage.setItem("preference", 3);
+    console.log(localStorage.getItem("preference"));
+  }
+};
+
+for (const themeRadio of themeRadioBtns) {
+  themeRadio.addEventListener("change", changeTheme);
+}
+
+// Storing the state of the theme locally
+const prevserveUserPreferance = () => {
+  const preference = localStorage.getItem("preference");
+  getStylesheet.setAttribute("href", `./dist/css/theme${preference}.min.css`);
+  themeRadioBtns[preference - 1].checked = true;
+};
+
+window.addEventListener("load", prevserveUserPreferance);
